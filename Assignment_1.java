2253d3ffc325370f482acbc9b8836da67ec1f29a
ArkEkum <Ekum.Ark@seattlecolleges.edu>
2025-07-17 15:25:20 -0700

    Add files via upload

---
+++ Assignment_1.java
@@ -0,0 +1,11 @@
+// Ekum Ark
+// CSC142
+// Assignment 1
+
+package assignment_1;
+public class Assignment_1 {
+	public static void main(String[] args) {
+		System.out.println("CSC142");
+		System.out.println("One thing I learned from module 1 is how to navigate and write code on the BlueJ and Eclipse IDE.");
+	}
+}
