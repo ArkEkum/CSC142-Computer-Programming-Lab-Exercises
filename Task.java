1e118b28f5aa7b08bc926d4fa2290ec6eb955a73
ArkEkum <Ekum.Ark@seattlecolleges.edu>
2025-08-26 13:57:16 -0700

    Task.java

--- Task.java
+++ Task.java
@@ -5,34 +5,60 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Task {
+	
 	private String title;
 	private LocalDate dueDate;
 	private boolean isCompleted;
+	
 	public Task(String title, LocalDate dueDate) {
+		
 		this.title = title;
 		this.dueDate = dueDate;
 		this.isCompleted = false;
+		
 	}
+	
 	public String getTitle() {	
+		
 		return title;
+		
 	}
-	public void setTitle(String title) {	
+	
+	public void setTitle(String title) {
+		
 		this.title = title;
+		
 	}
-	public LocalDate getDueDate() {	
+	
+	public LocalDate getDueDate() {
+		
 		return dueDate;
 	}
+	
 	public void setDueDate(LocalDate dueDate) {	
+		
 		this.dueDate = dueDate;
+		
 	}
+	
 	public boolean isCompleted() {	
+		
 		return isCompleted;
+		
 	}
+	
 	public void setCompleted(boolean completed) {	
+		
 		this.isCompleted = completed;
+		
 	}
+	
 	public String toString() {	
+		
 		String status = isCompleted ? "DONE" : "PENDING";
+		
 		return String.format("[%s] %s (Due: %s)", status, title, dueDate);
+		
 	}
+	
 }
