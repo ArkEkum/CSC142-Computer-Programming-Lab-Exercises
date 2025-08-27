03067cccf89f7956851b5ab9905084755971f9d3
ArkEkum <Ekum.Ark@seattlecolleges.edu>
2025-08-26 19:42:51 -0700

    Updated TaskController.java
    Updated after all working methods.

--- TaskController.java
+++ TaskController.java
@@ -11,76 +11,72 @@ class TaskController {
 	private List<Task> tasks = new ArrayList<>();
 	
 	public void addTask(String title, LocalDate dueDate) {
-		
 		tasks.add(new Task(title, dueDate));
-		int index = tasks.indexOf(title);
-		System.out.println("Task added: " + title + ", Due: " + dueDate + ", Index: " + index);
-		
+		System.out.println();
+		System.out.println("Task added: " + title + "; Due: " + dueDate);
 	}
 	
 	public void deleteTask(int index) {	
-		
 		if (index >= 0 && index < tasks.size()) {
-			
 			Task removed = tasks.remove(index);
+			System.out.println();
 			System.out.println("Task deleted: " + removed.getTitle());
-			
 		}
-		
 	}
 	
 	public void editTask(int index, String newTitle, LocalDate newDueDate) {
-		
 		if (index >= 0 && index < tasks.size()) {
-			
 			Task task = tasks.get(index);
 			task.setTitle(newTitle);
 			task.setDueDate(newDueDate);
-			System.out.println("Task updated: " + newTitle);
-			
+			System.out.println();
+			System.out.println("Task updated: " + newTitle + "; Due: " + newDueDate);
 		}
-		
 	}
 	
 	public void markTaskCompleted(int index) {	
-		
 		if (index >= 0 && index < tasks.size()) {
-			
 			tasks.get(index).setCompleted(true);
-			System.out.println("Task completed!");
-			
+			System.out.println();
+			System.out.println("Task Completed: " + tasks.get(index));
 		}
-		
 	}
 	
 	public List<Task> getAllTasks() {	
-		
 		List<Task> allTasks = new ArrayList<>(tasks);
-		
-		System.out.println("All Tasks: " + allTasks);
-		
+		System.out.println();
+		for (Task AllTasks : tasks) {
+			System.out.println(AllTasks);
+		}
 		return allTasks;
-		
-//		return new ArrayList<>(tasks);
-		
 	}
 	
-	public List<Task> getPendingTasks() {	
-		
-		return tasks.stream().filter(t -> !t.isCompleted()).collect(Collectors.toList());
-		
+	public List<Task> getPendingTasks() {
+		List<Task> pendingTasks = new ArrayList<>();
+		pendingTasks = tasks.stream().filter(t -> !t.isCompleted()).collect(Collectors.toList());
+		System.out.println();
+		for (Task PendingTasks : tasks) {
+			System.out.println(PendingTasks);
+		}
+		return pendingTasks;
 	}
-	
+
 	public List<Task> getCompletedTasks() {	
-		
-		return tasks.stream().filter(Task::isCompleted).collect(Collectors.toList());
-		
+		List<Task> completedTasks = new ArrayList<>();
+		completedTasks = tasks.stream().filter(Task::isCompleted).collect(Collectors.toList());
+		System.out.println();
+		for (Task CompletedTasks : tasks) {
+			System.out.println(CompletedTasks);
+		}
+		return completedTasks;
 	}
 	
 	public void sortByDueDate() {
-		
 		tasks.sort(Comparator.comparing(Task::getDueDate));
-		
+		System.out.println();
+		for (Task TasksByDueDate : tasks) {
+			System.out.println(TasksByDueDate);
+		}
 	}
 	
 }
