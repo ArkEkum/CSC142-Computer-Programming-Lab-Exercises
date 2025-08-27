a6fae170e0c1804753cada6e3dcdd6353d743ab1
ArkEkum <Ekum.Ark@seattlecolleges.edu>
2025-08-26 19:42:11 -0700

    Updated Task.java
    Updated after all working methods.

--- Task.java
+++ Task.java
@@ -11,54 +11,38 @@ class Task {
 	private boolean isCompleted;
 	
 	public Task(String title, LocalDate dueDate) {
-		
 		this.title = title;
 		this.dueDate = dueDate;
 		this.isCompleted = false;
-		
 	}
 	
 	public String getTitle() {	
-		
 		return title;
-		
 	}
 	
 	public void setTitle(String title) {
-		
 		this.title = title;
-		
 	}
 	
 	public LocalDate getDueDate() {
-		
 		return dueDate;
 	}
 	
 	public void setDueDate(LocalDate dueDate) {	
-		
 		this.dueDate = dueDate;
-		
 	}
 	
 	public boolean isCompleted() {	
-		
 		return isCompleted;
-		
 	}
 	
-	public void setCompleted(boolean completed) {	
-		
+	public void setCompleted(boolean completed) {
 		this.isCompleted = completed;
-		
 	}
 	
 	public String toString() {	
-		
 		String status = isCompleted ? "DONE" : "PENDING";
-		
 		return String.format("[%s] %s (Due: %s)", status, title, dueDate);
-		
 	}
 	
 }
