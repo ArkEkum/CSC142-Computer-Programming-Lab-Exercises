de6fc89356983c1a34f4dc43fbf6bdee637978ca
ArkEkum <Ekum.Ark@seattlecolleges.edu>
2025-08-27 19:59:45 -0700

    Updated TaskController.java
    Final version of "TaskController.java" for CSC142 Final Project.

--- TaskController.java
+++ TaskController.java
@@ -8,9 +8,9 @@ import java.util.stream.Collectors;
 
 class TaskController {
 	
-	private List<Task> tasks = new ArrayList<>();
+	private List<Task> tasks = new ArrayList<>(); // ArrayList created to store all tasks.
 	
-	private int normalizeIndex(int index) {
+	private int normalizeIndex(int index) { // Method sets negative index of tasks.
 		int size = tasks.size();
 		if (size == 0) {
 			return -1;
@@ -24,89 +24,89 @@ class TaskController {
 		return -1;
 	}
 	
-	public void addTask(String title, LocalDate dueDate) {
+	public void addTask(String title, LocalDate dueDate) { // Method adds task name and due date.
 		if (title == null || title.isBlank() || dueDate == null) {
-			System.out.println("Add Failed: Title and Due Date are required.");
+			System.out.println("Add Failed: Title and Due Date are required."); // Error message for empty task name/due date.
 			return;
 		}
-		tasks.add(new Task(title, dueDate));
+		tasks.add(new Task(title, dueDate)); // Task name and due date added into ArrayList.
 		System.out.println();
-		System.out.println("Task added: " + title + "; Due: " + dueDate);
+		System.out.println("Task added: " + title + "; Due: " + dueDate); // Displays added task name and due date.
 	}
 	
-	public void deleteTask(int index) {	
+	public void deleteTask(int index) {	// Method deletes task from ArrayList.
 		int i = normalizeIndex(index);
 		if (i == -1) {
-			System.out.println("Delete failed: No such task index (" + index + ").");
+			System.out.println("Delete failed: No such task index (" + index + ")."); // Error message for invalid task index.
 			return;
 		}
-		Task removed = tasks.remove(i);
+		Task removed = tasks.remove(i); // Task name and due date are removed from ArrayList.
 			System.out.println();
-			System.out.println("Task deleted: " + removed.getTitle());
+			System.out.println("Task deleted: " + removed.getTitle()); // Displays name of task deleted.
 	}
 	
-	public void editTask(int index, String newTitle, LocalDate newDueDate) {
+	public void editTask(int index, String newTitle, LocalDate newDueDate) { // Method edits task from ArrayList.
 		int i = normalizeIndex(index);
 		if (i == -1) {
-			System.out.println("Update failed: No such task index (" + index + ").");
+			System.out.println("Update failed: No such task index (" + index + ")."); // Error message for invalid task index.
 			return;
 		}
 		if (newTitle == null || newTitle.isBlank() || newDueDate == null) {
-			System.out.println("Update failed: Title and Due Date are required.");
+			System.out.println("Update failed: Title and Due Date are required."); // Error message for empty task name and due date.
 			return;
 		}
 		Task task = tasks.get(i);
-		task.setTitle(newTitle);
-		task.setDueDate(newDueDate);
+		task.setTitle(newTitle); // New task name added.
+		task.setDueDate(newDueDate); // New task due date added.
 		System.out.println();
-		System.out.println("Task updated: " + newTitle + "; Due: " + newDueDate);
+		System.out.println("Task updated: " + newTitle + "; Due: " + newDueDate); // Displays new task name and due date.
 	}
 	
-	public void markTaskCompleted(int index) {
+	public void markTaskCompleted(int index) { // Method marks task complete from ArrayList.
 		int i = normalizeIndex(index);
 		if (i == -1) {
-			System.out.println("Complete failed: No such task index (" + index + ").");
+			System.out.println("Complete failed: No such task index (" + index + ")."); // Error message for invalid task index.
 			return;
 		}
-		tasks.get(i).setCompleted(true);
+		tasks.get(i).setCompleted(true); // Task is set to complete.
 		System.out.println();
-		System.out.println("Task Completed: " + tasks.get(index));
+		System.out.println("Task Completed: " + tasks.get(index)); // Displays name of task completed.
 	}
 	
-	public List<Task> getAllTasks() {	
-		List<Task> allTasks = new ArrayList<>(tasks);
+	public List<Task> getAllTasks() { // Method gets all tasks from ArrayList.
+		List<Task> allTasks = new ArrayList<>(tasks); // Temporary ArrayList is created.
 		System.out.println();
 		for (Task AllTasks : tasks) {
-			System.out.println(AllTasks);
+			System.out.println(AllTasks); // Displays all tasks in ArrayList.
 		}
 		return allTasks;
 	}
 	
-	public List<Task> getPendingTasks() {
-		List<Task> pendingTasks = new ArrayList<>();
-		pendingTasks = tasks.stream().filter(t -> !t.isCompleted()).collect(Collectors.toList());
+	public List<Task> getPendingTasks() { // Method gets all incomplete tasks from ArrayList.
+		List<Task> pendingTasks = new ArrayList<>(); // Temporary ArrayList is created.
+		pendingTasks = tasks.stream().filter(t -> !t.isCompleted()).collect(Collectors.toList()); // All incomplete tasks are added into temporary ArrayList.
 		System.out.println();
 		for (Task PendingTasks : tasks) {
-			System.out.println(PendingTasks);
+			System.out.println(PendingTasks); // Displays all incomplete tasks in ArrayList.
 		}
 		return pendingTasks;
 	}
 
-	public List<Task> getCompletedTasks() {	
-		List<Task> completedTasks = new ArrayList<>();
-		completedTasks = tasks.stream().filter(Task::isCompleted).collect(Collectors.toList());
+	public List<Task> getCompletedTasks() { // Method gets all completed tasks from ArrayList.
+		List<Task> completedTasks = new ArrayList<>(); // Temporary ArrayList is created.
+		completedTasks = tasks.stream().filter(Task::isCompleted).collect(Collectors.toList()); // All completed tasks are added into temporary ArrayList.
 		System.out.println();
 		for (Task CompletedTasks : tasks) {
-			System.out.println(CompletedTasks);
+			System.out.println(CompletedTasks); // Displays all completed tasks in ArrayList.
 		}
 		return completedTasks;
 	}
 	
-	public void sortByDueDate() {
-		tasks.sort(Comparator.comparing(Task::getDueDate));
+	public void sortByDueDate() { // Method sorts all tasks in ArrayList by due date.
+		tasks.sort(Comparator.comparing(Task::getDueDate)); // All tasks are sorted by due date.
 		System.out.println();
 		for (Task TasksByDueDate : tasks) {
-			System.out.println(TasksByDueDate);
+			System.out.println(TasksByDueDate); // Displays all tasks by due date from ArrayList.
 		}
 	}
 	
