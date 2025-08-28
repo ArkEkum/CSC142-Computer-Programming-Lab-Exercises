12d7425a03ca69596529823c9802bf8538a3065c
ArkEkum <Ekum.Ark@seattlecolleges.edu>
2025-08-27 19:58:43 -0700

    Updated Task.java
    Final version of "Task.java" for CSC142 Final Project.

--- Task.java
+++ Task.java
@@ -10,37 +10,37 @@ class Task {
 	private LocalDate dueDate;
 	private boolean isCompleted;
 	
-	public Task(String title, LocalDate dueDate) {
+	public Task(String title, LocalDate dueDate) { // Method sets task name, due date, and completion status.
 		this.title = title;
 		this.dueDate = dueDate;
 		this.isCompleted = false;
 	}
 	
-	public String getTitle() {	
+	public String getTitle() { // Method gets task name.
 		return title;
 	}
 	
-	public void setTitle(String title) {
+	public void setTitle(String title) { // Method sets task name.
 		this.title = title;
 	}
 	
-	public LocalDate getDueDate() {
+	public LocalDate getDueDate() { // Method gets task due date.
 		return dueDate;
 	}
 	
-	public void setDueDate(LocalDate dueDate) {	
+	public void setDueDate(LocalDate dueDate) { // Method sets task due date.
 		this.dueDate = dueDate;
 	}
 	
-	public boolean isCompleted() {	
+	public boolean isCompleted() { // Method gets task completion status.
 		return isCompleted;
 	}
 	
-	public void setCompleted(boolean completed) {
+	public void setCompleted(boolean completed) { // Method sets task completion status.
 		this.isCompleted = completed;
 	}
 	
-	public String toString() {	
+	public String toString() { // Method displays task completion status.
 		String status = isCompleted ? "DONE" : "PENDING";
 		return String.format("[%s] %s (Due: %s)", status, title, dueDate);
 	}
